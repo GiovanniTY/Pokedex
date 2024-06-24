4234d9386595776a5c8831583c8d52508f83e2dc
Feys Dylan <148909357+HappyFeys@users.noreply.github.com>
2024-06-24 15:05:49 +0200

    scss des cards

--- engine.php
+++ engine.php
@@ -1,9 +1,8 @@
 <?php
 try {
     // Connexion database
-    $bdd = new PDO('mysql:host=localhost;dbname=pokedex;charset=utf8', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=pokedex;charset=utf8', 'root', '');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussi";
 } catch (PDOException $e) {
     echo "Erreur de connexion à la base de données: " . $e->getMessage();
     exit;
