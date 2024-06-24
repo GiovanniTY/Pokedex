13ea32c51a743f1262601154eb7bc54cad7d31e0
Justine-Frigo <justine0912@gmail.com>
2024-06-24 14:49:01 +0200

    variables d'environnement

--- login-register/config.php
+++ login-register/config.php
@@ -1,9 +1,15 @@
 <?php 
+use Dotenv\Dotenv;
 
-$host = 'localhost';
-$db = 'pokedex';
-$user = 'root';
-$password = 'root';
+require './vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$db = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 $url = 'mysql:host=' . $host . ';dbname=' . $db . ';charset=utf8';
 
