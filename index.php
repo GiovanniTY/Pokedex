df6c3058344068d383c132538d9f634e458c0c36
Giovanni_T <giovannitummino.15@gmail.com>
2024-06-24 13:59:06 +0200

    mise a jour

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
                     <img src="<?php echo $pokemon['image']; ?>" alt="<?php echo $pokemon['name']; ?>">
                     <h2><?php echo $pokemon['name']; ?></h2>
                     <p>ID: <?php echo $pokemon['pokedexId']; ?></p>
-                    <p>Tipo: <?php echo $pokemon['type1']; ?><?php if (!empty($pokemon['type2'])) echo ', ' . $pokemon['type2']; ?></p>
+                    <p>Tipe: <?php echo $pokemon['type1']; ?><?php if (!empty($pokemon['type2'])) echo ', ' . $pokemon['type2']; ?></p>
                 </div>
             <?php endforeach; ?>
         </div>
