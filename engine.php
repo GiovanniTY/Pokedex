88fbf354eba429a7ae61a60f4522c7b3fbfdeb00
Justine-Frigo <justine0912@gmail.com>
2024-06-25 09:38:34 +0200

    fix connection db

--- engine.php
+++ engine.php
@@ -1,9 +1,10 @@
 <?php
-try {
-    // Connexion database
-    $bdd = new PDO('mysql:host=localhost;dbname=pokedex;charset=utf8', 'root', 'root');
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Erreur de connexion à la base de données: " . $e->getMessage();
-    exit;
-}
+// try {
+//     // Connexion database
+//     $bdd = new PDO('mysql:host=localhost;dbname=pokedex;charset=utf8', 'root', 'root');
+//     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     echo "Erreur de connexion à la base de données: " . $e->getMessage();
+//     exit;
+// }
+include('./login-register/config.php'); ?>
--- search.php
+++ search.php
@@ -1,15 +1,16 @@
 <?php
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=pokedex;charset=utf8', 'root', 'root');
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Errore di connessione al database: " . $e->getMessage();
-    exit;
-}
+// try {
+//     $bdd = new PDO('mysql:host=localhost;dbname=pokedex;charset=utf8', 'root', 'root');
+//     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     echo "Errore di connessione al database: " . $e->getMessage();
+//     exit;
+// }
+include('./login-register/config.php');
 
 if (isset($_GET['search'])) {
     $searchTerm = htmlspecialchars($_GET['search']);
-    $stmt = $bdd->prepare('SELECT * FROM Pokemon WHERE name LIKE :name');
+    $stmt = $pdo->prepare('SELECT * FROM Pokemon WHERE name LIKE :name');
     $stmt->execute(['name' => $searchTerm . '%']);
     $pokemonList = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
