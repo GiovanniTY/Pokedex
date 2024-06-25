e72d48d01256511558d6bc256694c0b7448d119a
Justine-Frigo <justine0912@gmail.com>
2024-06-25 12:36:17 +0200

    desktop style for forms

--- login-register/profile.php
+++ login-register/profile.php
@@ -65,43 +65,57 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <title>Profile</title>
 </head>
 
-<body>
-    <h1>Profile</h1>
-    <img src="<?php echo '.' . $_SESSION['user']['avatar'] ?>" class="avatar-img" alt="avatar" id="profile-img">
-    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
-        <label for="avatar">Avatar</label>
-        <select name="avatar" id="avatar">
-            <option <?php echo $_SESSION['user']['avatar'] === './storage/default_avatars/pikachu.png' ? 'selected' : '' ?> value="./storage/default_avatars/pikachu.png">Pikachu</option>
-            <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/bulbizarre.png' ? 'selected' : ''?> value="./storage/default_avatars/bulbizarre.png">Bulbizarre</option>
-            <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/caninos.png' ? 'selected' : ''?> value="./storage/default_avatars/caninos.png">Caninos</option>
-            <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/carapuce.png' ? 'selected' : ''?> value="./storage/default_avatars/carapuce.png">Carapuce</option>
-            <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/goupix.png' ? 'selected' : ''?> value="./storage/default_avatars/goupix.png">Goupix</option>
-            <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/salameche.png' ? 'selected' : ''?> value="./storage/default_avatars/salameche.png">Salamèche</option>
-        </select><br>
-        <label for="username">Username</label>
-        <input type="text" name="username" value="<?php echo $_SESSION['user']['username'] ?>"><br>
-        <?php if (isset($errors['username_missing'])) { ?><span class="error" id="username_error"><?php echo $errors['username_missing'] ?? ''; ?></span><br><?php } ?>
-        <label for="description">Description</label>
-        <textarea name="description" id="description" rows="5"><?php echo $_SESSION['user']['description'] ?></textarea><br>
-        <label for="email">Email</label>
-        <input type="email" name="email" value="<?php echo $_SESSION['user']['email'] ?>"><br>
-        <?php if (isset($errors['email_missing'])) { ?><span class="error" id="email_error"><?php echo $errors['email_missing'] ?? ''; ?></span><br><?php } ?>
-        <?php if (isset($errors['email_invalid'])) { ?><span class="error" id="email_invalid"><?php echo $errors['email_invalid'] ?? ''; ?></span><br><?php } ?>
-        <label for="password">Modify your password</label>
-        <input type="password" name="password"><br>
-        <?php if (isset($errors['password_identical'])) { ?><span class="error" id="password_identical"><?php echo $errors['password_identical'] ?? ''; ?></span><br><?php } ?>
+<body class="body-backend">
+    <nav>
+        <a href="../index.php">Back to home</a>
+    </nav>
+    <form class="form-backend" action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+        <h1>Profile</h1>
+        <img src="<?php echo '.' . $_SESSION['user']['avatar'] ?>" class="avatar-img" alt="avatar" id="profile-img">
+        <div>
+            <label for="avatar">Avatar</label>
+            <select name="avatar" id="avatar">
+                <option <?php echo $_SESSION['user']['avatar'] === './storage/default_avatars/pikachu.png' ? 'selected' : '' ?> value="./storage/default_avatars/pikachu.png">Pikachu</option>
+                <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/bulbizarre.png' ? 'selected' : '' ?> value="./storage/default_avatars/bulbizarre.png">Bulbizarre</option>
+                <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/caninos.png' ? 'selected' : '' ?> value="./storage/default_avatars/caninos.png">Caninos</option>
+                <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/carapuce.png' ? 'selected' : '' ?> value="./storage/default_avatars/carapuce.png">Carapuce</option>
+                <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/goupix.png' ? 'selected' : '' ?> value="./storage/default_avatars/goupix.png">Goupix</option>
+                <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/salameche.png' ? 'selected' : '' ?> value="./storage/default_avatars/salameche.png">Salamèche</option>
+            </select>
+        </div>
+        <div>
+            <label for="username">Username</label>
+            <input type="text" name="username" value="<?php echo $_SESSION['user']['username'] ?>">
+            <?php if (isset($errors['username_missing'])) { ?><span class="error" id="username_error"><?php echo $errors['username_missing'] ?? ''; ?></span><?php } ?>
+        </div>
+        <div>
+            <label for="description">Description</label>
+            <textarea name="description" id="description" rows="5"><?php echo $_SESSION['user']['description'] ?></textarea>
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" value="<?php echo $_SESSION['user']['email'] ?>">
+            <?php if (isset($errors['email_missing'])) { ?><span class="error" id="email_error"><?php echo $errors['email_missing'] ?? ''; ?></span><?php } ?>
+            <?php if (isset($errors['email_invalid'])) { ?><span class="error" id="email_invalid"><?php echo $errors['email_invalid'] ?? ''; ?></span><?php } ?>
+        </div>
+        <div>
+            <label for="password">Modify your password</label>
+            <input type="password" name="password">
+            <?php if (isset($errors['password_identical'])) { ?><span class="error" id="password_identical"><?php echo $errors['password_identical'] ?? ''; ?></span><?php } ?>
+        </div>
         <button type="submit" name="submit">Update</button>
     </form>
 
-    <script> 
+    <script>
         const img = document.getElementById("profile-img");
         const select = document.getElementById('avatar');
         select.addEventListener('change', function() {
-        img.src = '.' + select.value;
-    });
+            img.src = '.' + select.value;
+        });
     </script>
 </body>
 
