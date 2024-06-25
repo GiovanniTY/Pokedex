60a92f97a893c73c83759b4cf5dd69f9924aad4e
Justine-Frigo <justine0912@gmail.com>
2024-06-25 11:33:09 +0200

    profile almost done

--- login-register/profile.php
+++ login-register/profile.php
@@ -4,11 +4,57 @@ include('../login-register/config.php');
 
 session_start();
 
-if (isset($_POST['submit'])){
-    $username = $_POST['username'];
-    $description = $_POST['description'] ?? null;
-    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-    $password = $_POST['password'] ?? $_SESSION['user']['password'];
+if (isset($_POST['submit'])) {
+    $errors = [];
+    $username = $_POST['username'] ?? '';
+    $description = $_POST['description'] ?? PDO::PARAM_NULL;
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] != '' ? $_POST['password'] : $_SESSION['user']['password'];
+    $avatar = $_POST['avatar'];
+
+    if ($username === '') {
+        $errors['username_missing'] = 'The username is required. We have set it back to your original one.';
+    }
+    if ($email === '') {
+        $errors['email_missing'] = 'The email is required. We have set it back to your original one.';
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email_invalid'] = 'The email is invalid. We have set it back to your original one.';
+    }
+    if (password_verify($password, $_SESSION['user']['password'])) {
+        $errors['password_identical'] = 'Your new password must be different.';
+    }
+
+    if (empty($errors)) {
+        try {
+            $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "UPDATE users SET username = :username, email = :email, password = :password, avatar = :avatar, description = :description WHERE id = :id;";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(":username", $username);
+            $stmt->bindParam(":email", $email);
+            $stmt->bindParam(":password", $password);
+            $stmt->bindParam(":avatar", $avatar);
+            $stmt->bindParam(":description", $description);
+            $stmt->bindParam(":id", $_SESSION['user']['id']);
+            $stmt->execute();
+        } catch (PDOException $error) {
+            print_r($error->getMessage());
+        }
+
+        try {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+            $statement->bindParam(':email', $email);
+            $statement->execute();
+
+
+            $_SESSION['user'] = $statement->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $error) {
+            print_r($error->getMessage());
+        }
+    }
 }
 
 ?>
@@ -24,26 +70,30 @@ if (isset($_POST['submit'])){
 
 <body>
     <h1>Profile</h1>
+    <img src="<?php echo '.' . $_SESSION['user']['avatar'] ?>" class="avatar-img" alt="avatar">
     <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
         <label for="avatar">Avatar</label>
         <select name="avatar" id="avatar">
-            <option value="../storage/default_avatars/pikachu.png">Pikachu</option>
-            <option value="../storage/default_avatars/bulbizarre.png">Bulbizarre</option>
-            <option value="../storage/default_avatars/caninos.png">Caninos</option>
-            <option value="../storage/default_avatars/carapuce.png">Carapuce</option>
-            <option value="../storage/default_avatars/goupix.png">Goupix</option>
-            <option value="../storage/default_avatars/salameche.png">Salamèche</option>
+            <option <?php echo $_SESSION['user']['avatar'] === './storage/default_avatars/pikachu.png' ? 'selected' : '' ?> value="./storage/default_avatars/pikachu.png">Pikachu</option>
+            <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/bulbizarre.png' ? 'selected' : ''?> value="./storage/default_avatars/bulbizarre.png">Bulbizarre</option>
+            <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/caninos.png' ? 'selected' : ''?> value="./storage/default_avatars/caninos.png">Caninos</option>
+            <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/carapuce.png' ? 'selected' : ''?> value="./storage/default_avatars/carapuce.png">Carapuce</option>
+            <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/goupix.png' ? 'selected' : ''?> value="./storage/default_avatars/goupix.png">Goupix</option>
+            <option <?php echo $_SESSION['user']['avatar'] ===  './storage/default_avatars/salameche.png' ? 'selected' : ''?> value="./storage/default_avatars/salameche.png">Salamèche</option>
         </select><br>
         <label for="username">Username</label>
-        <input type="text" name="username" value="<?php echo $_SESSION['user']['username']?>"><br>
+        <input type="text" name="username" value="<?php echo $_SESSION['user']['username'] ?>"><br>
+        <?php if (isset($errors['username_missing'])) { ?><span class="error" id="username_error"><?php echo $errors['username_missing'] ?? ''; ?></span><br><?php } ?>
         <label for="description">Description</label>
-        <textarea name="description" id="description" rows="5" value="<?php echo $_SESSION['user']['description']?>"></textarea><br>
+        <textarea name="description" id="description" rows="5" value="<?php echo $_SESSION['user']['description'] ?>"></textarea><br>
         <label for="email">Email</label>
-        <input type="email" name="email" value="<?php echo $_SESSION['user']['email']?>"><br>
+        <input type="email" name="email" value="<?php echo $_SESSION['user']['email'] ?>"><br>
+        <?php if (isset($errors['email_missing'])) { ?><span class="error" id="email_error"><?php echo $errors['email_missing'] ?? ''; ?></span><br><?php } ?>
+        <?php if (isset($errors['email_invalid'])) { ?><span class="error" id="email_invalid"><?php echo $errors['email_invalid'] ?? ''; ?></span><br><?php } ?>
         <label for="password">Modify your password</label>
         <input type="password" name="password"><br>
+        <?php if (isset($errors['password_identical'])) { ?><span class="error" id="password_identical"><?php echo $errors['password_identical'] ?? ''; ?></span><br><?php } ?>
         <button type="submit" name="submit">Update</button>
-
     </form>
 </body>
 
