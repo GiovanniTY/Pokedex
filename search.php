634e1ecfd1c11b82e20c298de054c0f705b9440a
Giovanni_T <giovannitummino.15@gmail.com>
2024-06-26 09:15:06 +0200

    mise a jour

--- search.php
+++ search.php
@@ -2,20 +2,20 @@
 include('./login-register/config.php'); 
 
 $per_page = 20; 
-$page = isset($_GET['page']) ? (int)$_GET['page'] : 1; // Pagina corrente
-$offset = ($page - 1) * $per_page; // Calcolo dell'offset per la query
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1; 
+$offset = ($page - 1) * $per_page; 
 
-// Verifica se esiste un termine di ricerca
+// Check if a search term exists
 $searchTerm = isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '';
 
 try {
-    // Query per contare il numero totale di risultati (senza LIMIT e OFFSET)
+   // Query to count the total number of results (without LIMIT and OFFSET)
     $countStmt = $pdo->prepare('SELECT COUNT(*) FROM Pokemon WHERE name LIKE :name');
     $countStmt->bindValue(':name', $searchTerm . '%', PDO::PARAM_STR);
     $countStmt->execute();
     $total_count = $countStmt->fetchColumn();
     
-    // Query per selezionare i Pokémon con paginazione
+    // Query to select Pokémon with pagination
     $stmt = $pdo->prepare('SELECT * FROM Pokemon WHERE name LIKE :name LIMIT :per_page OFFSET :offset');
     $stmt->bindValue(':name', $searchTerm . '%', PDO::PARAM_STR);
     $stmt->bindValue(':per_page', $per_page, PDO::PARAM_INT);
@@ -27,6 +27,6 @@ try {
     exit;
 }
 
-// Calcolo del numero totale di pagine
+// Calculating the total number of pages
 $total_pages = ceil($total_count / $per_page);
 ?>
