cccb9b62b7a1992167dbd00628a82a665cc3411d
Feys Dylan <148909357+HappyFeys@users.noreply.github.com>
2024-06-26 16:57:57 +0200

    commit de fin de journée

--- compare.php
+++ compare.php
@@ -48,13 +48,13 @@ try {
         <button id="dark-mode-toggle">
             <img id="pokeball" src="assets/img/pokeball2.png" alt="pokeball">
         </button>
-        <h1>Compare tes Pokemon</h1>
+        <h1>Compare tes Pokemons</h1>
         <div id="zone__fight">
             <div class="plateforme plateforme--left"></div>
             <div class="plateforme plateforme--right"></div>
         </div>
         <div id="compare__container">
-            <div id="compare__left">
+            <div id="compare__left"></div>
             <div id="compare__right"></div>
         </div>
     </main>
