cd19ba12f7d7d3663a4c17acde517af38b2c4393
Giovanni_T <giovannitummino.15@gmail.com>
2024-06-27 11:21:47 +0200

    mise a jour

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,14 @@ import { compare } from './component/compare.js';
 import {favorite} from './component/favorite.js';
 import { filter } from "./component/filter.js";
 import { setupSearch } from './component/search.js';
+import { toggleDarkMode } from './component/darkmode.js';
 
+
+setupSearch()
 
 toggleDarkMode()
+
 favorite()
 filter()
 compare()
-
-
-
-
-
 
